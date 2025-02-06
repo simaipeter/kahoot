97e3e60650b0f2266088e2093097c1042de0430c
simaipeter <simaipeter@gmail.com>
2025-02-06 08:35:09 +0100

    v1.01

--- index.js
+++ index.js
@@ -5,18 +5,33 @@ function init() {
     div.setAttribute("style", "position:absolute; top:0px; left:0px; width:100vw; background:#ffaaaa; z-index:1000000");
     div.innerHTML = "Automata mód";
     document.body.append(div);
+    var counter = document.createElement("div");
+    counter.id = "counter_spl";
+    counter.setAttribute("style", "position:absolute; top:200px; left:calc(50% - 25vw); width:50vw; z-index:1000000; text-align:center; color:white; font-size:200pt;");
+    counter.innerHTML = "";
+    document.body.append(counter);
+    test();
 
 }
 
-function displayCounter() {
-
+function displayCounter(inp) {
+    var counter = document.querySelector("#counter_spl");
+    counter.innerHTML = inp;
+    if ( inp > 0 ) {
+        setTimeout(function() {displayCounter(inp-1);}, 1000);
+    }
+    else {
+        document.querySelector("div[class*='top-bar'] button:not([class*='skip'])").click();
+        counter.innerHTML = "";
+        setTimeout(test, 500);
+    }
+    
 }
 
 function test() {
     if (document.querySelector("div[class*='answers-histogram']") != null || document.querySelector("div[class*='scoreboard']") != null) {
-        document.querySelector("div[class*='top-bar'] button:not([class*='skip'])").click();
         console.log('ok');
-        setTimeout(test, 1500);
+        displayCounter(3);
     }
     else {
         setTimeout(test, 500);
